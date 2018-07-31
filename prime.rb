221aa85b933640ebd49cca53ef1a40c3f5770cfc
Alison Shin <alisonyshin@gmail.com>
2018-07-31 05:25:05 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,9 @@ def prime?(possibly_prime_number)
   numbers_range = (2..possibly_prime_number).to_a
   numbers_range.all? {|number| 
     while number < possibly_prime_number
-      if possibly_prime_number % number != 0
+      possibly_prime_number % number != 0
     end
   end
-    #where number != possibly_prime_number
   }
 end
 puts prime?(9)

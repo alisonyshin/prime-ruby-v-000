34bc646fa21aeae700378b18db2bbe14bea3772d
Alison Shin <alisonyshin@gmail.com>
2018-07-31 05:36:04 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -6,18 +6,3 @@ def prime?(possibly_prime_number)
     return false
   end
 end
-
-puts prime?(-1)
-
-
-def prime?(num)
-  if num < 0 or num == 0 or num == 1
-    return false
-  else
-    (2..num-1).to_a.all? do |possible_factor|
-      num % possible_factor != 0
-    end
-  end
-end
-
-puts prime?(105557)

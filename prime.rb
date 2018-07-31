45e807932bb6d829c3c3e88e6c035c10b8f55ab1
Alison Shin <alisonyshin@gmail.com>
2018-07-31 05:19:41 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(possibly_prime_number)
+  #numbers_range = (2..10).to_a
+  numbers_range = [2,3]
+  numbers_range.all? {|number| 
+    if number != possibly_prime_number
+      if possibly_prime_number % number != 0
+    end
+  end
+    #where number != possibly_prime_number
+  }
+end
+puts prime?(10)
